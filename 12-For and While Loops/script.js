8854140da5f350877867b98dbc6605acc8f8ac34
Beastman-dev <wmeastman@gmail.com>
2025-03-27 17:19:36 -0400

    Completed module 13-Arrays

---
+++ 12-For and While Loops/script.js
@@ -0,0 +1,18 @@
+let population = 100;
+
+// population = population * 1.05;
+
+/*let year = 0;
+
+while(year < 10) {
+    population = population * 1.05;
+    year++;
+    console.log(population);
+}
+*/
+
+for(let i = 0; i < 10; i++) {
+    population = population * 1.05;
+}
+
+console.log(population);
